4402f04d525bd7ae4fe70158e35727386fe7200f
Marat <user@LAPTOP-7JKVIGGR>
2017-07-18 18:54:20 +0500

    Код написан и прокомментирован. Теоритические данные присутсвуют, как и объяснение их отличия от практических

---
+++ Practice 2017 12/BinTree.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice_2017_12
+{
+    class BinTree
+    {
+        public int Data;      // Информационное поле
+        public BinTree Left;  // Ссылка на левую ветку
+        public BinTree Right; // Ссылка на правую ветку
+
+        public BinTree(int d)
+        {
+            Data = d;
+            Left = null;
+            Right = null;
+        }
+    } // Класс бинарного дерева
+}
